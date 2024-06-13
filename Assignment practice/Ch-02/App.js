fa1ddf26f9fde6ae5282ff4fa9b4dfa73f24d4cd
adityaranjan858 <adityaranjan858@gmail.com>
2024-06-14 03:14:47 +0530

    Updated Designs

--- Assignment practice/Ch-02/App.js
+++ Assignment practice/Ch-02/App.js
@@ -1,9 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
+const heading = React.createElement("div", {  }, [
+  React.createElement("h2", { id: "heading" }, "This heading is representing by :"),
+  React.createElement(
+    "h2",
+    { class: "heading" },
+    "'My Own Create React App'"
+  ),
+  React.createElement(
+    "h2",
+    { class: "sub_heading" },
+    "Used :"
+  ),
+  React.createElement(
+    "ul",
+    { class: "items" },
+    [React.createElement("li", {class: "item_list"}, "npm"),
+        React.createElement("li", {class: "item_list"}, "parcel bundler"),
+        React.createElement("li", {class: "item_list"}, "react & react-dom"),
+        React.createElement("li", {class: "item_list"}, ".gitignore"),
+    ]
+    
+  ),
+]);
 
-const heading = React.createElement("h1",{id : "heading"}, "This heading is representing by 'My Own Create React App'.")
-
-const root = ReactDOM.createRoot(document.getElementById("root"))
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(heading);
